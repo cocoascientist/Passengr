c5ecf4cbcab308b6aca863c59ca178718e6d4d71
Andrew Shepard <shep.andy@gmail.com>
2015-11-30 12:48:53 -0800

    Add core data

---
+++ Passengr/Pass+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Pass+CoreDataProperties.swift
+//  Passengr
+//
+//  Created by Andrew Shepard on 11/30/15.
+//  Copyright © 2015 Andrew Shepard. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Pass {
+
+    @NSManaged var name: String?
+    @NSManaged var url: String?
+
+}
---
+++ Passengr/Pass.swift
@@ -0,0 +1,17 @@
+//
+//  Pass.swift
+//  Passengr
+//
+//  Created by Andrew Shepard on 11/30/15.
+//  Copyright © 2015 Andrew Shepard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Pass: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
