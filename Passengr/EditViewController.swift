412500cc8e9fc7383614c5efd0b4596e8be0db84
Andrew Shepard <shep.andy@gmail.com>
2015-11-30 12:40:26 -0800

    Add edit view controller

--- Passengr/EditViewController.swift
+++ Passengr/EditViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
+private let reuseIdentifier = "PassEditCell"
+
 class EditViewController: UITableViewController {
+    
+    private var passes: [String] {
+        return ["Blewett", "Cayuse", "Chinook", "Disautel", "Manastash", "Sherman", "Snoqualmie", "Stevens", "Wauconda", "White"]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.tableView.editing = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,71 +30,84 @@ class EditViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.passes.count
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        var cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier)
+        
+        if cell == nil {
+            cell = newCellForIndexPath(indexPath)
+        }
+        
+        guard let _ = cell else { fatalError() }
+        
+        configureCell(cell!, forIndexPath: indexPath)
+        
+        return cell!
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
+    
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+        // TODO: reorder passes
     }
-    */
+    
+    override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        return .None
+    }
+    
+    override func tableView(tableView: UITableView, shouldIndentWhileEditingRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+    
+    // MARK: - Actions
+
     @IBAction func handleDoneButton(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    @IBAction func handleEnableSwitchChange(sender: AnyObject) {
+        guard let enableSwitch = sender as? UISwitch else { return }
+        let index = enableSwitch.tag - 1
+        let _ = self.passes[index]
+        
+//        pass.enabled = enableSwitch.on
+    }
+    
+    // MARK: - Private
+    
+    private func newCellForIndexPath(indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: reuseIdentifier)
+        
+        let enableSwitch = UISwitch()
+        let switchSize = enableSwitch.sizeThatFits(CGSizeZero)
+        enableSwitch.frame = CGRectMake(cell.contentView.bounds.width - switchSize.width - 15.0,
+            (cell.contentView.bounds.height - switchSize.height) / 2.0,
+            switchSize.width, switchSize.height)
+        
+        enableSwitch.tag = indexPath.row + 1
+        enableSwitch.autoresizingMask = UIViewAutoresizing.FlexibleLeftMargin
+        enableSwitch.addTarget(self, action: "handleEnableSwitchChange:", forControlEvents: .ValueChanged)
+        
+        cell.contentView.addSubview(enableSwitch)
+        
+        return cell
+    }
+    
+    private func configureCell(cell: UITableViewCell, forIndexPath indexPath: NSIndexPath) {
+        let pass = passes[indexPath.row]
+        
+        cell.textLabel?.text = pass
+        
+        if let _ = cell.contentView.viewWithTag(indexPath.row + 1) as? UISwitch {
+//                        enableSwitch.on = pass.enabled.boolValue
+        }
+    }
 }
