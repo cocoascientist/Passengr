64a5b2e7a10a01c091fad7da422ddf563ab8caf6
Andrew Shepard <shep.andy@gmail.com>
2015-12-02 09:45:23 -0800

    Add Pass Edit cell

---
+++ Passengr/PassEditCell.swift
@@ -0,0 +1,26 @@
+//
+//  PassEditCell.swift
+//  Passengr
+//
+//  Created by Andrew Shepard on 12/1/15.
+//  Copyright © 2015 Andrew Shepard. All rights reserved.
+//
+
+import UIKit
+
+class PassEditCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var swtch: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
