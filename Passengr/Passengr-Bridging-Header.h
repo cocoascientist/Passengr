cf442427b6b54796b4e2016bd23e19c579c53c58
Andrew Shepard <shep.andy@gmail.com>
2015-12-11 19:54:33 -0800

    Use #include instead of #import

--- Passengr/Passengr-Bridging-Header.h
+++ Passengr/Passengr-Bridging-Header.h
@@ -2,8 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <libxml/HTMLtree.h>
-#import <libxml/HTMLparser.h>
-#import <libxml/xpath.h>
-#import <libxml/xpathInternals.h>
-#import <libxml/xmlerror.h>
+#include <libxml/HTMLtree.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+#include <libxml/xmlerror.h>
